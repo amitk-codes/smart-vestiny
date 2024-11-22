639b61cb37a482e88e714c31452c3226a2256113
Amit Kumar <amitkcodes@gmail.com>
2024-11-22 21:41:29 +0530

    created the CreateVestingAccount struct for create_vesting_account instruction

--- programs/smart_vestiny/src/lib.rs
+++ programs/smart_vestiny/src/lib.rs
@@ -1,16 +1,55 @@
 use anchor_lang::prelude::*;
 
+pub mod constants;
+pub mod state;
+
+use anchor_spl::token_interface::{Mint, TokenAccount, TokenInterface};
+pub use constants::*;
+pub use state::*;
+
 declare_id!("98vJxJanGrYXsw22T8ERBweXvPWLEQwsxbpXAjiFdeKf");
 
 #[program]
 pub mod smart_vestiny {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_vesting_account(
+        ctx: Context<CreateVestingAccount>,
+        _company_name: String,
+    ) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(company_name: String)]
+pub struct CreateVestingAccount<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        init,
+        payer = signer,
+        space = ANCHOR_DISCRIMINATOR + VestingAccount::INIT_SPACE,
+        seeds = [company_name.as_ref(), signer.key().as_ref()],
+        bump,
+    )]
+    pub vesting_account: Account<'info, VestingAccount>,
+
+    pub mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        init,
+        payer = signer,
+        token::mint = mint,
+        token::authority = treasury_token_account,
+        seeds = [b"treasury_token_account", company_name.as_bytes()],
+        bump,
+    )]
+    pub treasury_token_account: InterfaceAccount<'info, TokenAccount>,
+
+    pub system_program: Program<'info, System>,
+
+    pub token_program: Interface<'info, TokenInterface>,
+}
