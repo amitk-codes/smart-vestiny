f444fd3d62b7bc44a448cde75fbbb5758c89e11f
Amit Kumar <amitkcodes@gmail.com>
2024-11-22 22:44:53 +0530

    added a new account state: EmployeeAccount

--- programs/smart_vestiny/src/state/account_states.rs
+++ programs/smart_vestiny/src/state/account_states.rs
@@ -12,3 +12,15 @@ pub struct VestingAccount {
     pub treasury_bump: u8,
     pub bump: u8,
 }
+
+#[account]
+#[derive(InitSpace)]
+pub struct EmployeeAccount {
+    pub beneficiary: Pubkey,
+    pub total_amount: u64,
+    pub total_withdrawal_account: u64,
+    pub start_time: i64,
+    pub end_time: i64,
+    pub cliff_time: i64, // the period after which the employee can withdraw the tokens
+    pub vesting_account: Pubkey, // storing the vesting_account as well to have an reference
+} 
